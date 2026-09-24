be7f2d760ee1afe40be1c2a57f1da8cc57cdf0a0
Pjjush16 <pjjush16@users.noreply.github.com>
2026-09-24 19:09:05 +0800

    v4.2: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hud.navi"
         minSdk = 21
         targetSdk = 34
-        versionCode = 10
-        versionName = "4.0"
+        versionCode = 42
+        versionName = "4.2"
     }
 
     buildTypes {
